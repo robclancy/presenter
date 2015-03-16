74064ab71d18b2dac2c5240aa52fe7c48d5914dd
Christopher Roussel <christopher@impleri.net>
2015-03-15 22:19:05 -0500

    Updating stub to be compatible with parent

--- tests/FactoryStub.php
+++ tests/FactoryStub.php
@@ -1,6 +1,7 @@
 <?php
 
 use Mockery as m;
+use Illuminate\View\View;
 use Robbo\Presenter\View\Factory;
 
 class FactoryStub extends Factory
@@ -12,7 +13,7 @@ class FactoryStub extends Factory
         return m::mock('Illuminate\View\Engines\EngineInterface');
     }
 
-    public function callCreator($view)
+    public function callCreator(View $view)
     {
         return $view;
     }
