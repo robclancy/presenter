da4bb9ebbb1abd96a0e42ff50e34c16cc1259cf0
Robbo <robbo.clancy@gmail.com>
2017-02-22 14:30:09 +1000

    run php-cs-fixer

--- src/View/View.php
+++ src/View/View.php
@@ -1,21 +1,22 @@
-<?php namespace Robbo\Presenter\View;
+<?php
 
-use Robbo\Presenter\Presenter;
+namespace Robbo\Presenter\View;
+
 use Illuminate\View\View as BaseView;
 
-class View extends BaseView {
-
+class View extends BaseView
+{
     /**
      * Add a piece of data to the view.
      *
-     * @param  string|array  $key
-     * @param  mixed   $value
+     * @param string|array $key
+     * @param mixed        $value
+     *
      * @return \Illuminate\View\View
      */
     public function with($key, $value = null)
     {
-        if (is_array($key))
-        {
+        if (is_array($key)) {
             return parent::with($this->factory->decorate($key));
         }
 
