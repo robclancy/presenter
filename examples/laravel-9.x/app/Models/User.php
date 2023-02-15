fbee454317eef985b68e54647d26293116aa92be
robclancy <robbo.clancy@gmail.com>
2023-02-15 13:25:54 +1000

    updated dev env and added laravel 9 example/test

---
+++ examples/laravel-9.x/app/Models/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Presenters\UserPresenter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Robbo\Presenter\PresentableInterface;
+
+class User extends Authenticatable implements PresentableInterface
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getPresenter()
+    {
+        return new UserPresenter($this);
+    }
+}
