05ffb0c08696e68ad8a82d56aa7edbfca26c262d
robclancy <robbo.clancy@gmail.com>
2024-02-22 01:20:44 +1000

    fixed (suppressed) deprecation warnings from a PHP 8.1 change and updated phpunit. fixes #66

--- src/Presenter.php
+++ src/Presenter.php
@@ -72,6 +72,7 @@ abstract class Presenter implements \ArrayAccess
      * @param mixed $offset
      * @return boolean
      */
+    #[\ReturnTypeWillChange]
     public function offsetExists($offset)
     {
         // We only check isset on the array, if it is an object we return true as the object could be overloaded
@@ -94,6 +95,7 @@ abstract class Presenter implements \ArrayAccess
      * @param  mixed  $offset
      * @return mixed
      */
+    #[\ReturnTypeWillChange]
     public function offsetGet($offset)
     {
         return $this->__get($offset);
@@ -105,6 +107,7 @@ abstract class Presenter implements \ArrayAccess
      * @param  mixed  $offset
      * @param  mixed  $value
      */
+    #[\ReturnTypeWillChange]
     public function offsetSet($offset, $value)
     {
         if (is_array($this->object)) {
@@ -121,6 +124,7 @@ abstract class Presenter implements \ArrayAccess
      *
      * @param  mixed  $offset
      */
+    #[\ReturnTypeWillChange]
     public function offsetUnset($offset)
     {
         if (is_array($this->object)) {
--- tests/autoload.php
+++ tests/autoload.php
@@ -1,4 +1,6 @@
 <?php
 
+error_reporting(E_ALL);
+
 require __DIR__.'/helpers.php';
 require __DIR__.'/../vendor/autoload.php';
